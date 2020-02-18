8daf6367551b3593f2c1969fe966c54e37d86e71
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2020-02-18 15:35:37 +0900

    Add dedicated newsletter

---
+++ component---src-templates-all-tags-index-js-ea60fd7ba0fe89487605.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{YSlf:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Wbzz");t.default=function(e){var t=e.pageContext.tags;return l.a.createElement("div",{style:{fontFamily:"avenir"}},l.a.createElement("div",null,l.a.createElement("ul",null,t.map((function(e,t){return l.a.createElement("li",{key:t},l.a.createElement(r.a,{to:"/tags/"+e},e))})))))}}}]);
+//# sourceMappingURL=component---src-templates-all-tags-index-js-ea60fd7ba0fe89487605.js.map
